da8e698c8253a64607e32f74ec7f1f302c6e23a2
Sally Rademacher <srademacher@umass.edu>
2023-08-01 22:47:22 -0400

    combined weather retrieval with text send - no errors, but text isn't going thru

--- getWeather.js
+++ getWeather.js
@@ -1,13 +1,16 @@
 import fetch from 'node-fetch';
 import queryString from 'query-string';
 import moment from 'moment';
+import 'dotenv/config';
+import twilio from 'twilio';
 
-
+const accountSid = "ACbb34c3f41f7d330761d1143bdea610e3";
+const authToken = "264901e9129ebef5484444d6225af406";
 // set the Timelines GET endpoint as the target URL
 const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
 
 // get your key from app.tomorrow.io/development/keys
-const apikey = process.env.TOMORROW_API_KEY;
+const apikey = "blcaNlcJOvisvQEAf3idR5GpbBdFc27N";
 const amherst = [42.373222, -72.519852];
 // amherst, ma
 let location = amherst;
@@ -54,7 +57,21 @@ fetch(getTimelineURL + "?" + getTimelineParameters, {method: "GET", compress: tr
     let data = json.data.timelines[0].intervals[0].values
     let currentTemp = data.temperature
     let precip = data.precipitationType
+
+    const client = twilio(accountSid, authToken);
+
+    client.messages
+    .create({
+        body: `It is currently ${currentTemp} degrees out.`,
+        from: "+18668285812",
+        to: '+17814057882'
+    })
+    .then(message => console.log(message.sid));
+
     console.log(currentTemp)
     })
-  .catch((error) => console.error("error: " + err));
 
+  .catch((err) => console.error("error: " + err));
+
+
+  
