5e80116d37118dc325d346e401125b71fd227c55
Sally Rademacher <srademacher@umass.edu>
2023-08-07 18:28:29 -0400

    moved server.js logic to index.js, deleted server.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import path from 'path';
 import {fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+
+import twilio from 'twilio';
+import './getWeather.js';
+
+const { MessagingResponse } = twilio.twiml;
 
 const app = express();
 const router = express.Router();
@@ -12,6 +18,29 @@ const __dirname = path.dirname(__filename);
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.post('/', async function(req, res){
+    try {
+
+        const twiml = new MessagingResponse();
+
+        let weatherData = await getWeatherData(getTimelineURL, getTimelineParameters)
+.then(data => {
+
+            twiml.message(`It is ${data} degrees out.`)
+            res.type('text/xml').send(twiml.toString());
+        })
+
+        
+
+    }catch(error){
+        console.log("error")
+    }
 });
 
 //add router
